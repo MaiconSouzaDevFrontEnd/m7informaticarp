e5ca5a461bb81259096ce99bfbb3f6668175d82e
MaiconSouzaDevFrontEnd <dev.front.end.maiconsouza@gmail.com>
2024-12-19 06:54:28 -0300

    criando buttons

--- javascrip.js
+++ javascrip.js
@@ -1,6 +1,6 @@
 const bntDetails = document.querySelector(".buttonDetails");
 const cardBack = document.querySelector(".cardBack");
 function backCard(){
-    cardBack.classList.add('backStyle');
+    cardBack.style.top = "0px";
 }
 bntDetails.addEventListener('click', backCard);
